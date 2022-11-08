cebaca24d25d3d20f15fc72e9f52748c5127f70c
dafenghuang <dafenghuang@tencent.com>
2022-11-08 18:09:17 +0800

    refract directory structure

--- mihoyobbs/uitrace/test_mys.py
+++ mihoyobbs/uitrace/test_mys.py
@@ -1,9 +1,6 @@
-import os
-import time
-
 from uitrace.api import *
 
-import settings
+from common import settings
 
 
 class TestHoyo(object):
@@ -20,16 +17,17 @@ class TestHoyo(object):
     def teardown_method(self, method):
         screenshot(label="finishForCheck", img_path=None, pos=None)
         logger.info("用例执行完成，杀app进程")
-        # stop_app(settings.APPNAME)
+        stop_app(settings.APPNAME)
 
-    def test_login(self):
-        if find(loc="//*[@resource-id='android:id/content']/android.view.ViewGroup[1]/android.widget.TextView[4]", by=DriverType.UI, timeout=5):
-            click(loc="//*[@resource-id='android:id/content']/android.view.ViewGroup[1]/android.widget.TextView[4]", by=DriverType.UI, offset=None, timeout=30, duration=0.05, times=1)
+    def test_enter_login(self):
+        if find(loc="//*[@resource-id='android:id/content']/android.view.ViewGroup[1]/android.widget.TextView[4]",
+                by=DriverType.UI, timeout=5):
+            click(loc="//*[@resource-id='android:id/content']/android.view.ViewGroup[1]/android.widget.TextView[4]",
+                  by=DriverType.UI, offset=None, timeout=30, duration=0.05, times=1)
             time.sleep(3)
         else:
             logger.info("没有找到同意并继续")
             # raise Exception("没有找到同意并继续")
-
         if find(loc="obj_1667550112916.jpg", by=DriverType.CV, timeout=10):
             logger.info("当前正在 请选择你关注的游戏页面")
             logger.info("找到了选好了， 准备点击")
@@ -45,9 +43,9 @@ class TestHoyo(object):
         else:
             logger.info("没有找到青少年模式的我知道了确认")
 
-
         # 接下来要确认是否到达了首页，方法很简单，找一下是否有签到福利
-        if find(loc="//*[@resource-id='com.mihoyo.hyperion:id/mGoldenPart']/androidx.recyclerview.widget.RecyclerView[1]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]/android.widget.ImageView[1]", by=DriverType.UI, timeout=30):
+        if find(loc="//*[@resource-id='com.mihoyo.hyperion:id/mGoldenPart']/androidx.recyclerview.widget.RecyclerView[1]/android.widget.LinearLayout[2]/android.widget.RelativeLayout[1]/android.widget.ImageView[1]",
+                by=DriverType.UI, timeout=30):
             logger.info("找到了签到福利，成功到达首页")
         else:
             logger.info("没有找到签到福利")
@@ -60,11 +58,14 @@ class TestHoyo(object):
 
         if find("//*[@resource-id='com.mihoyo.hyperion:id/mHomePageRbMe']/android.view.ViewGroup[1]", by=DriverType.UI):
             logger.info("找到了 我的")
-            click(loc="//*[@resource-id='com.mihoyo.hyperion:id/mHomePageRbMe']/android.view.ViewGroup[1]", by=DriverType.UI, offset=None, timeout=30, duration=0.05, times=1)
+            click(loc="//*[@resource-id='com.mihoyo.hyperion:id/mHomePageRbMe']/android.view.ViewGroup[1]",
+                  by=DriverType.UI, offset=None, timeout=30, duration=0.05, times=1)
             # 查找点击登录
             # 如果有新手提示
             times = 1
-            while not find(loc="//*[@resource-id='com.mihoyo.hyperion:id/rootLayout']/android.view.ViewGroup[1]/android.widget.TextView[1]", by=DriverType.UI, timeout=30):
+            while not find(
+                    loc="//*[@resource-id='com.mihoyo.hyperion:id/rootLayout']/android.view.ViewGroup[1]/android.widget.TextView[1]",
+                    by=DriverType.UI, timeout=5):
                 if times >= 5:
                     logger.info("find 5 times, 点击登录failed")
                     raise Exception("找到点击登录按钮失败")
@@ -73,11 +74,15 @@ class TestHoyo(object):
                 times += 1
 
             times = 1
-            while not find(loc="//*[@resource-id='android:id/content']/android.view.ViewGroup[1]/android.widget.FrameLayout[1]/android.view.ViewGroup[1]", by=DriverType.UI, timeout=30):
+            while not find(
+                    loc="//*[@resource-id='android:id/content']/android.view.ViewGroup[1]/android.widget.FrameLayout[1]/android.view.ViewGroup[1]",
+                    by=DriverType.UI, timeout=5):
                 if times >= 5:
                     logger.info("cannot click 点击登录")
                     raise Exception("无法点击 点击登录并进入登陆页面")
-                click(loc="//*[@resource-id='com.mihoyo.hyperion:id/rootLayout']/android.view.ViewGroup[1]/android.widget.TextView[1]", by=DriverType.UI, offset=None, timeout=30, duration=0.05, times=1)
+                click(
+                    loc="//*[@resource-id='com.mihoyo.hyperion:id/rootLayout']/android.view.ViewGroup[1]/android.widget.TextView[1]",
+                    by=DriverType.UI, offset=None, timeout=30, duration=0.05, times=1)
                 time.sleep(1)
                 times += 1
 
@@ -85,6 +90,4 @@ class TestHoyo(object):
             logger.info("没有找到我的")
             raise Exception("首页上没有我的按钮")
 
-       
         logger.info("成功到达登陆页面")
-        
